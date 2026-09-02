aa4d06919fb58500fb69422c745a3dc29bdd1265
FirstMistborn <220653144+FirstMistborn@users.noreply.github.com>
2026-09-02 21:43:34 +1200

    Add Lume guided massage PWA

---
+++ sw.js
@@ -0,0 +1,36 @@
+const VERSION = "lume-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(VERSION).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== VERSION).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((res) => {
+        const copy = res.clone();
+        caches.open(VERSION).then((cache) => cache.put(event.request, copy));
+        return res;
+      }).catch(() => caches.match("./index.html"));
+    })
+  );
+});
